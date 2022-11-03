5aa8a942ab21b2bcdfb41e4a5190fdd63d33ce99
Bill Herrick <wherrickjr@gmail.com>
2022-11-03 13:45:58 -0500

    final edits

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -1,6 +1,39 @@
+
+# representation of movies rented
 the_little_mermaid = 3
 brother_bear = 5
 hercules = 1
 
 price = 3 * (the_little_mermaid + brother_bear + hercules)
 print(price)
+
+#how much payment working at companies
+google = 400
+amazon = 380
+facebook = 350
+
+total_salary = 10 * facebook + 6 * google + 4 * amazon
+print(total_salary)
+
+## if a student can be enrolled
+class_full = False
+
+class_schedule = True
+
+class_full == False and class_schedule == True
+
+## whether or not an offer can be applied
+(number_of_items > 2 or premium_member == True ) and offer_expired == False
+
+
+username = 'codeup'
+password = 'notastrongpassword'
+
+len(password) >= 5
+len(username) <= 20
+username != password
+username[0] != ' '
+password[0] != ' '
+
+
+
